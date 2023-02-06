0789c2a0bd16b8e054f66e40310cbdcf875539c0
deepamkr <deepamkumar@quaratech.com>
2023-02-06 17:14:04 +0530

    Second

---
+++ seeders/users.ts
@@ -0,0 +1,16 @@
+import { v4 as uuidv4} from 'uuid';
+
+export const users=[
+    {
+        id : uuidv4(),
+        namee: 'john',
+        email: 'john@gmail.com',
+        password: '123'
+    },
+    {
+        id : uuidv4(),
+        namee: 'shamy',
+        email: 'shamy@gmail.com',
+        password: '12345'
+    }
+]
